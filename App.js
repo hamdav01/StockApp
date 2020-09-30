f66130edb70f34520a837583b4a22cf86c3e9ea4
Hampus Davidsson <hampus.davidsson@gmail.com>
2020-09-30 19:07:57 +0200

    added first working fetch

--- App.js
+++ App.js
@@ -2,11 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const myKEY = '0e9d6faee7msha19b9b9ffebf94fp1551d4jsn34f915bc0afa';
+
 export default function App() {
+  // fetch(
+  //   'https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?region=US&q=tesla',
+  //   {
+  //     method: 'GET',
+  //     headers: {
+  //       'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+  //       'x-rapidapi-key': '0e9d6faee7msha19b9b9ffebf94fp1551d4jsn34f915bc0afa',
+  //     },
+  //   }
+  // )
+  //   .then((response) => {
+  //     console.log(response.json());
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
